9a828e6ca3bd74094e2709cf72e66e128d71b59e
jaypokharna <pokharnajay@gmail.com>
2024-02-04 03:15:50 +0530

    .env

--- server/Model/room.js
+++ server/Model/room.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema;
 require('dotenv').config();
 
-mongoose.connect(process.env.MONGO_URL)
+mongoose.connect(process.env.MONGO_URL+'/codelive')
 
 const roomSchema = new Schema({
 
