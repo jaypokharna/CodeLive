72061ad8ed6df1f72e3688d5e391be2f628de8da
jaypokharna <pokharnajay@gmail.com>
2024-02-04 04:02:05 +0530

    Minor Changes

--- server/index.js
+++ server/index.js
@@ -21,11 +21,10 @@ app.use(cors(
 
 // Middleware to set CORS headers
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', 'https://code-live-plum.vercel.app');
-  // You can set other CORS headers as needed
+  res.setHeader('Access-Control-Allow-Origin', 'https://code-live-plum.vercel.app/');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-  res.setHeader('Access-Control-Allow-Credentials', true);
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Credentials', 'true');
   next();
 });
 
